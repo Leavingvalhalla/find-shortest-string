1e197ba887f723c3e8fdb38bdaedd0738b42babd
Zach Lefler <leaving.valhalla@gmail.com>
2022-04-01 09:08:01 -0700

    initial commit

--- javascript/find_shortest_string.js
+++ javascript/find_shortest_string.js
@@ -1,21 +1,30 @@
 function findShortestString(arr) {
-  // type your code here
+  let shortest = arr[0];
+  arr.forEach((word) => {
+    if (word.length < shortest.length) {
+      shortest = word;
+    }
+  });
+  return shortest;
 }
 
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: 'a'");
-  console.log("=>", findShortestString(['aaa', 'a', 'bb', 'ccc']));
+  console.log('=>', findShortestString(['aaa', 'a', 'bb', 'ccc']));
 
-  console.log("");
+  console.log('');
 
   console.log("Expecting: 'hi'");
-  console.log("=>", findShortestString(['cat', 'hi', 'dog', 'an']));
+  console.log('=>', findShortestString(['cat', 'hi', 'dog', 'an']));
 
-  console.log("");
+  console.log('');
 
   console.log("Expecting: 'lily'");
-  console.log("=>", findShortestString(['flower', 'juniper', 'lily', 'dadelion']));
+  console.log(
+    '=>',
+    findShortestString(['flower', 'juniper', 'lily', 'dadelion'])
+  );
 
   // BENCHMARK HERE
 }
@@ -23,4 +32,12 @@ if (require.main === module) {
 module.exports = findShortestString;
 
 // Please add your pseudocode to this file
+// shortest word = arr[0]
+// for word in arr:
+// if word shorter than shortest word:
+// shortest word = word
+// return shortest word
+
 // And a written explanation of your solution
+
+// Iterate over the array and compare each word to the current shortest word.
